de50d1c734d1687d43e27a1b9f8152a92f18d60b
unknown <santanuign2011@gmail.com>
2026-05-21 00:01:19 +0530

    first commit

---
+++ src/test/java/com/example/testdocker/testdocker/TestdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdocker.testdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
